5ad2ba797cdeb29942c164f17f2434a2ac21ac83
ben <ben@localhost.localdomain>
2017-06-09 23:41:11 +0100

    Moved project specifics to a specerate manifest file. Final python executable should be project independent.

--- python_automation/run_hls.py
+++ python_automation/run_hls.py
@@ -8,17 +8,29 @@ Created on Fri Jun  9 08:03:07 2017
 
 import os
 
-project_name = os.path.relpath(".","..")
-top_level_function_name = "simple_adder"
-src_dir_name = "src"
-tb_dir_name = "tb"
-src_files = ["dut.h","dut.cpp"]
-tb_files = ["testbench.cpp"]
-part_name = "xc7z020clg484-1"
-clock_period = "10"
+# Load project specifics from local manifest file
+from Manifest import *
 
+# Set some default values if not specified in manifest file
+try:
+    src_dir_name
+except:
+    src_dir_name = 'src'
+
+try:
+    tb_dir_name
+except:
+    tb_dir_name = 'tb'
+
+# Set the project name
+try:
+    project_name
+except:
+    project_name = "proj_" + os.path.relpath(".","..")
+
+# Write out TCL file
 file = open("run_hls_python_output.tcl","w")
-file.write("open_project proj_" + project_name + "\n")
+file.write("open_project " + project_name + "\n")
 file.write("set_top " + top_level_function_name + "\n")
 for src_file in src_files:
     file.write("add_files " + src_dir_name + "/" + src_file + "\n")
@@ -33,4 +45,5 @@ file.write("cosim_design -O -rtl vhdl" + "\n")
 file.write("exit" + "\n")
 file.close()
 
+# Call the Vivado HLS process
 os.system("vivado_hls -f run_hls_python_output.tcl")
